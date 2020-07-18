9a8033a1e0e51fb2616380542f114e127bad5853
Simon Plakolb <s.plakolb@gmail.com>
2020-07-18 15:54:29 +0200

    Add documentation

--- src/Foreign/Hasky.hs
+++ src/Foreign/Hasky.hs
@@ -1,4 +1,17 @@
-module Foreign.Hasky where
+{- |
+Module          : Foreign.Hasky
+Description     : Haskell backend for Python's Hasky
+Copyright       : (c) Simon Plakolb, 2020
+License         : LGPLv3
+Maintainer      : s.plakolb@gmail.com
+Stability       : beta
+
+    This package contains the Haskell backend to the Python package 'Hasky'. It contains wrapper functions and can create FFI bindings from a Haskell source file. The Python and Haskell package are closely related. Some of the provided wrapper types may have unconventional design due to limitations of Python's 'ctypes' library.
+    This package is mainly provided to give developers the possibility to exporiment with Hasky's types within a Haskell environment. The Python library contains the source files as well and will compile them itself in a local directory.
+ -}
+module Foreign.Hasky (
+    createFileBindings
+) where
 
 import Text.Parsec.String (parseFromFile)
 import Text.Parsec.Error (ParseError)
@@ -8,6 +21,12 @@ import Foreign.Hasky.ParseTypes (parseTypeDefs, TypeDef(funcN))
 import Foreign.Hasky.ParseExports (parseExports, parseModname)
 import Foreign.Hasky.FFICreate (createFFI)
 
+{- |
+    Parses a Haskell source file at @fp@ and creates a new module for which it will return the @FilePath@.
+    The new file will be located in the same diretory and contain @foreign export ccall@s for all those functions, where wrapping is possible.
+    Lists are converted to @Foreign.Hasky.Arrayæs, Strings also get their own type. This is due to the handling of these types by Python's 'ctypes'. Tuples with up to four fields can also be wrapped as C structs using the @c-structs@ package.
+    It is not necessary to use @Foreign.C.Types@. Conversion to these types will be done automatically. It is however necessary to provide type definitions for all functions that should be in the exports. Type synonyms and custom types are not (yet) supported.
+ -}
 createFileBindings :: FilePath -> IO FilePath
 createFileBindings fp = do
     modn <- parseFromFile parseModname fp
--- src/Foreign/Hasky/List.hs
+++ src/Foreign/Hasky/List.hs
@@ -1,4 +1,14 @@
-module Foreign.Hasky.List (CList, newList, peekList, withList, freeList, fromList) where
+{- |
+Module          : Foreign.Hasky.List
+Description     : Linked List type for Python's interfacing library Hasky
+Copyright       : (c) Simon Plakolb, 2020
+License         : LGPLv3
+Maintainer      : s.plakolb@gmail.com
+Stability       : beta
+
+    Legacy type and functions for linked lists as interface for Haskell lists to Python sequences. The relevant conversion and parsin functions in the Python part of Hasky should still be able to handle a @CList@. It is recommended to wrap lists as @Foreign.Hasky.Array@. Use linked lists only if you have a good reason to do so.
+ -}
+module Foreign.Hasky.List (CList, newList, peekList, freeList) where
 
 import Foreign.Ptr
 import Foreign.Storable (Storable, peek)
@@ -6,12 +16,15 @@ import Foreign.Marshal.Utils (new)
 import Foreign.Marshal.Alloc (free)
 import Foreign.C.Structs (Struct2(..))
 
+-- | Type synonym for a pointer to a struct with a field of type @a@ and a pointer to another such struct. The final element contains a null pointer in the second field.
 type CList a = Ptr (Struct2 a (CList a))
 
+-- | Allocates space for a linked list while building it from a Haskell list of @Storable@s. The @CList@ has to be freed after its use with @freeList@.
 newList :: (Storable a) => [a] -> IO (CList a)
 newList [] = return nullPtr
 newList (x:xs) = newList xs >>= new . Struct2 x
 
+-- | (Re-)Creates a Haskell list out of a @CList@. Memory is not freed within this function. If it had been allocated within Haskell it needs to be freed with @freeList@.
 peekList :: (Storable a) => CList a -> IO [a]
 peekList lp = do
     le <- peek lp
@@ -23,11 +36,7 @@ peekList lp = do
         li <- peekList n
         return (x:li)
 
-withList :: Storable a => CList a -> ([a] -> [a]) -> IO (CList a)
-withList cl f = do
-    xs <- peekList cl
-    newList $ f xs
-
+-- | Frees all memory allocated for a @CList@ by @newList@.
 freeList :: (Storable a) => CList a -> IO ()
 freeList lp = do
     le <- peek lp
